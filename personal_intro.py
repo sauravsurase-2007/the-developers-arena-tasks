2a7d294ebd183f6a1d977094a746fe1d8ef02c4b
Saurav Surase <sauravsurase@gmail.com>
2026-06-13 13:07:38 +0530

    Add project files

---
+++ personal_intro.py
@@ -0,0 +1,14 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your hobby: ")
+
+print("\n===== Welcome =====")
+print(f"Name : {name}")
+print(f"Age  : {age}")
+print(f"Hobby: {hobby}")
+
+print(f"\nWelcome, {name}! We are happy to have you here.")
+print(f"It's great that you enjoy {hobby}.")
+print("Nice to meet you!")
+print("I am happy to work with the Developers Arena ")
